435a74280326f97b3738b40c29462372b2f78d73
LilyCole <Lilia.Cole@gmail.com>
2016-12-13 15:09:36 -0800

    adds resume and skills section

--- scripts/script.js
+++ scripts/script.js
@@ -13,3 +13,24 @@ $(function() {
     }
   });
 });
+
+$(document).ready( function () {
+
+  var modal = $('#myModal');
+  var btn = $('#myBtn');
+  var span = $(document).find('.close');
+
+  btn.on('click', function() {
+      modal.css('display','block');
+  });
+
+  span.on('click', function() {
+      modal.css('display','none');
+  });
+
+  $(window).on('click', function(event) {
+      if (event.target == modal) {
+          modal.css('display','none');
+      }
+  });
+});
